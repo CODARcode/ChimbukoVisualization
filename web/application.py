187c8ad57c8c7efef8e45847e36e8427648a248e
wyjeong <jeong.oneyoung@gmail.com>
2018-10-05 13:56:38 -0400

    adjust sampling rate to 1

--- web/application.py
+++ web/application.py
@@ -32,7 +32,7 @@ class Data(object):
             "fidx": [],
             "tidx": 0
         };
-        self.sampling_rate = 0.001;
+        self.sampling_rate = 1;
         self.sampling_strategy = ["uniform"]
         self.layout = ["entry", "value", "comm ranks", "exit"] # feild no.1, 2, ..
         self.log = []

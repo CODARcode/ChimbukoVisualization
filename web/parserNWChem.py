09b02c987607f6ac6bc4252200aac157fed69754
wyjeong <jeong.oneyoung@gmail.com>
2018-10-09 11:54:54 -0400

    add dynamic foi on parserNWChem

--- web/parserNWChem.py
+++ web/parserNWChem.py
@@ -2,7 +2,7 @@ import requests
 import time
 import json
 
-vis_url = 'http://127.0.0.1:5000/events'
+vis_url = 'http://0.0.0.0:5000/events'
 vis_data = "../data/StreamingNWChem/"
 
 #----set function dictionary----
@@ -10,12 +10,6 @@ fun_names = []
 with open(vis_data+"function.json", 'r') as f:
 	fun_names = json.load(f)
 requests.post(vis_url, json={'type':'functions', 'value':fun_names})
-
-#----set function of interest----
-foi = []
-with open(vis_data+"foi.0.json", 'r') as f:
-	foi = json.load(f)
-requests.post(vis_url, json={'type':'foi','value':foi})
 
 #----set event types, they are not fixed----
 et = []
@@ -36,23 +30,30 @@ event_list.sort(key=lambda x: int(x.split('.')[-2]))
 anomaly_list = glob.glob(vis_data+"anomaly.*.json")
 anomaly_list.sort(key=lambda x: int(x.split('.')[-2]))
 
+foi_list = glob.glob(vis_data+"foi.*.json")
+foi_list.sort(key=lambda x: int(x.split('.')[-2]))
+
 for i in range(len(event_list)):
+	
+	#----set function of interest----
+	foi = []
+	with open(foi_list[i], 'r') as f:
+		foi = json.load(f)
+	
 	labels = []
 	with open(anomaly_list[i], 'r') as f:
 		labels = json.load(f)
-	print("send ", len(labels)," anomaly data")
-	requests.post(vis_url, json={'type':'labels', 'value':labels})
-	time.sleep(0.1)
-
-	all_events = []
+	
+	events = []
 	with open(event_list[i], 'r') as f:
-		all_events = json.load(f)
+		events = json.load(f)
 
-	# send events data
-	print("send ", len(all_events)," events data")
-	res = requests.post(vis_url, json={'type':'events','value':all_events})
+	res = requests.post(vis_url, json={'type':'info','value':{
+		"events": events,
+		"foi": foi,
+		"labels": labels
+	}})
 	print(res.json())
-	time.sleep(1)
 
 # requests.post('http://127.0.0.1:5000/log', json={'type':'log'})
 

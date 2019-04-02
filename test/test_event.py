c4862bc48e5a778169eb425b1d36d837b2c284e6
wyjeong <wonyong.jeong@stonybrook.edu>
2019-04-02 13:45:24 -0400

    Modified prev test cases / Added new unit test

--- test/test_event.py
+++ test/test_event.py
@@ -4,71 +4,74 @@ sys.path.insert(0,'../web/')
 import time
 import json
 import glob
-import data_manager as dm
 import unittest
+import data_manager as dm
 
-test_data = "./data/2x2x2/2cores/" 
-test_data_broken = "./data/2x2x2/2cores/broken/" 
+test_data = "./data/aggregated/events/" 
 
-def prep_data(d):
-    et = []
-    fun_names = []
-    with open(test_data+"function.json", 'r') as f:
-        fun_names = json.load(f)
-    with open(test_data+"et.json", 'r') as f:
-        et = json.load(f)
-    
-    d.reset()
-    d.set_event_types(et)
-    d.set_functions(fun_names)
+func = []
+with open(test_data+"function.json", 'r') as f:
+    func = json.load(f)
+
+et = []
+with open(test_data+"et.json", 'r') as f:
+    et = json.load(f)
 
 class TestEvent(unittest.TestCase):
-    
+   
+    def prep_data(self, d):
+        d.reset()
+
     def test_add_events(self):
         """
-        Test that the new events are correctly parsed.
+        Check if events trace data is correctly parsed.
         """
         d = dm.Data()
-        prep_data(d);
+        self.prep_data(d);
 
         event_list = glob.glob(test_data+"trace.*.json")
         event_list.sort(key=lambda x: int(x.split('.')[-2]))
     
         for i in range(len(event_list)):
-            info = []
+            trace = []
             with open(event_list[i], 'r') as f:
-                info = json.load(f)
-            d.set_FOI(info['value']['foi'])
-            d.set_labels(info['value']['labels'])
-            d.add_events(info['value']['events'])
+                trace = json.load(f)['value']
+            d.set_event_types(et)
+            d.set_functions(func)
+            d.set_FOI(trace['foi'])
+            d.set_labels(trace['labels'])
+            d.add_events(trace['events'])
         
-        i=0
-        ans = [177367, 216175]
-
-        for k in d.events.keys():
-            self.assertEqual(len(d.events[k]), ans[i])
-            i += 1
-
-        
-    def test_add_events_broken(self):
+        ans = {
+            0.0: 216175,
+            1.0: 177367
+        }
+        for rank, ev in d.events.items():
+            # print(rank, len(ev))
+            self.assertEqual(len(ev), ans[rank])
+    
+    def test_num_anomaly(self):
         """
-        Test that the broken events are safely detected.
+        Check if anomalies are correctly parsed.
         """
         d = dm.Data()
-        prep_data(d);
+        self.prep_data(d);
 
-        event_list = glob.glob(test_data_broken+"trace.*.json")
+        event_list = glob.glob(test_data+"trace.*.json")
         event_list.sort(key=lambda x: int(x.split('.')[-2]))
     
         for i in range(len(event_list)):
-            info = []
+            trace = []
             with open(event_list[i], 'r') as f:
-                info = json.load(f)
-            d.set_FOI(info['value']['foi'])
-            d.set_labels(info['value']['labels'])
-            self.assertRaises(Exception, d.add_events, info['value']['events'])
-            break
-           
+                trace = json.load(f)['value']
+            d.set_event_types(et)
+            d.set_functions(func)
+            d.set_FOI(trace['foi'])
+            d.set_labels(trace['labels'])
+            d.add_events(trace['events'])
+        
+        ans = 299
+        self.assertEqual(d.anomaly_cnt, ans)
  
 if __name__ == '__main__':
     unittest.main()
---
+++ test/test_execution.py
@@ -0,0 +1,97 @@
+import sys
+sys.path.insert(0,'../web/')
+
+import time
+import json
+import glob
+import data_manager as dm
+import unittest
+import threading
+
+NUM_RANK = 8
+DATA_PATH_AGGR = "./data/aggregated/executions/" 
+DATA_PATH_RANK = "./data/per_rank/" 
+global_data = {} # global
+
+def check_data():
+    cnt = 0
+    for rank, data in global_data.items():
+        cnt += len(data)
+    if cnt == 0:
+        return False
+    else: 
+        return True
+
+def send_data(rank, data):
+    try:
+        while len(global_data[rank])>0:
+            d = global_data[rank].pop(0)
+            print(threading.current_thread().getName(), ': adding ', len(d['executions']), 'executions')
+            data.set_statistics(d['stat'])
+            data.add_executions(d['executions'])
+    except:
+        print(threading.current_thread().getName(), ': error occured')
+
+class TestExecution(unittest.TestCase):
+    
+    def prep_data(self, d):
+        d.reset()
+
+    def test_add_executions(self):
+        """
+        Test that the forest are correctly generated.
+        """
+        d = dm.Data()
+        self.prep_data(d);
+
+        event_list = glob.glob(DATA_PATH_AGGR+"trace.*.json")
+        event_list.sort(key=lambda x: int(x.split('.')[-2]))
+    
+        for i in range(len(event_list)):
+            data = []
+            with open(event_list[i], 'r') as f:
+                data = json.load(f)
+            d.set_statistics(data['stat'])
+            d.add_executions(data['executions'])
+        ans = 1059
+        self.assertEqual(len(d.executions.keys()), ans)
+
+    def test_add_executions_by_rank(self):
+        """
+        Test that the forest is not generated safely.
+        """
+        d = dm.Data()
+        self.prep_data(d);
+
+        event_list = glob.glob(DATA_PATH_RANK+"trace.*.json")
+        event_list.sort(key=lambda x: int(x.split('.')[-2]))
+        
+        for i in range(NUM_RANK):
+            path_list = glob.glob(DATA_PATH_RANK+'rank.'+str(i)+'.execution.*.json')
+            path_list.sort(key=lambda x: int(x.split('.')[-2]))
+            for path in path_list:
+                with open(path, 'r') as f:
+                    if i not in global_data:
+                        global_data[i] = []
+                    global_data[i].append(json.load(f))
+        try:
+            threads = []
+            for i in range(NUM_RANK):
+                # _thread.start_new_thread(send_data, ('tid-'+str(i), i, d) )
+                t = threading.Thread(target=send_data, args=(i, d))
+                threads.append(t)
+                t.start()
+        except:
+            print ('Error: unable to start thread')
+        while check_data():
+            pass
+
+        print('Simulation Done', len(d.executions))
+        
+        for thread in threads:
+            thread.join()
+        
+        self.assertEqual(len(d.executions), 2118)
+ 
+if __name__ == '__main__':
+    unittest.main()

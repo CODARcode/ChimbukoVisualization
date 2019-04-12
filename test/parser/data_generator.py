f56b6afc2e765819491323e65427b58f97f67ff1
wyjeong <wonyong.jeong@stonybrook.edu>
2019-04-12 14:09:10 -0400

    Updated stream parser and data generator

--- test/parser/data_generator.py
+++ test/parser/data_generator.py
@@ -1,65 +1,202 @@
+import sys
+sys.path.insert(0, '../../')
+
 import requests
 import time
 import json
 import glob
+from module.DataManager import DataManager
 
-mode = 'executions' 
-vis_url = 'http://0.0.0.0:5000/'
-vis_data = "../data/aggregated/executions/"
+MODE = 'events' # events or executions
+DATA_PATH = "../../data/nwchem_outdated/" 
 
-# load data
-data_list = glob.glob(vis_data+"trace.*.json")
-data_list.sort(key=lambda x: int(x.split('.')[-2]))
-
-eidmap = {}
-eids = {}
-filecnt = 0
-for i in range(len(data_list)):
-    data = []
-    
-    with open(data_list[i], 'r') as f:
-        data = json.load(f)
-    executions = data['executions']
-    frame_data = {}
-    for eid, execution in executions.items():
-        
-        execution['comm ranks'] = execution['comm ranks'] + 4 # if you want to increase rank data
-        
-        rankno = execution['comm ranks'] 
-        
-        if rankno not in frame_data:
-            frame_data[rankno] = {}
-        if rankno not in eidmap:
-            eidmap[rankno] = {}
-        if rankno not in eids:
-            eids[rankno] = -1
-        
-        if execution['findex'] not in eidmap:
-            eidmap[execution['findex']] = eids[rankno]
-            eids[rankno] -= 1
-        execution['findex'] = eidmap[execution['findex']]
-        
-        if execution['parent'] not in eidmap:
-            eidmap[execution['parent']] = eids[rankno]
-            eids[rankno] -= 1
-        execution['parent'] = eidmap[execution['parent']]
-        
-        new_children = []
-        for cid in execution['children']:
-            if cid not in eidmap:
-                eidmap[cid] = eids[rankno]
-                eids[rankno] -= 1
-            new_children.append(eidmap[cid])
-        execution['children'] = new_children
-        
-        frame_data[rankno][eidmap[eid]] = execution
-
-    for rank, rank_data in frame_data.items():
+def update_old_events_to_trace_format(): # ../../data/nwchem_outdated/
+    """
+    Reformat outdated data format to merged trace format
+    """
+    func = []
+    with open(DATA_PATH+"function.json", 'r') as f:
+        func = json.load(f)
+    et = []
+    with open(DATA_PATH+"et.json", 'r') as f:
+        et = json.load(f)
+    trace_list = glob.glob(DATA_PATH+"trace.*.json")
+    trace_list.sort(key=lambda x: int(x.split('.')[-2]))
+    foi_list = glob.glob(DATA_PATH+"foi.*.json")
+    foi_list.sort(key=lambda x: int(x.split('.')[-2]))
+    anomaly_list = glob.glob(DATA_PATH+"anomaly.*.json")
+    anomaly_list.sort(key=lambda x: int(x.split('.')[-2]))
+    for i in range(len(trace_list)):
+        foi = []
+        with open(foi_list[i], 'r') as f:
+            foi = json.load(f)
+        trace = []
+        with open(trace_list[i], 'r') as f:
+            trace = json.load(f)
+        anomaly = []
+        with open(anomaly_list[i], 'r') as f:
+            anomaly = json.load(f)
+        trace.sort(key=lambda event:event[11])
         j = json.dumps({
-            'executions': rank_data,
-            'stat': data['stat']
+            'type': 'info',
+            'value': {
+                'events': trace,
+                'foi': foi,
+                'functions': func,
+                'event_types': et,
+                'labels': anomaly
+            }
         })
-        f = open('rank.'+str(int(rank))+'.execution.'+str(filecnt)+'.json','w')
+        f = open('trace.'+str(i)+'.json','w')
         f.write(j)
         f.close()
-    filecnt += 1
+
+def convert_events_to_executions(): # ../../data/nwchem_outdated/
+    """
+    Convert events to executions
+    """
+    data_manager = DataManager()
+    data_manager.reset()
+
+    func = []
+    with open(DATA_PATH+"function.json", 'r') as f:
+        func = json.load(f)
+    data_manager.set_functions(func)
+
+    et = []
+    with open(DATA_PATH+"et.json", 'r') as f:
+        et = json.load(f)
+    data_manager.set_event_types(et)
+
+    trace_list = glob.glob(DATA_PATH+"trace.*.json")
+    trace_list.sort(key=lambda x: int(x.split('.')[-2]))
+
+    foi_list = glob.glob(DATA_PATH+"foi.*.json")
+    foi_list.sort(key=lambda x: int(x.split('.')[-2]))
+
+    anomaly_list = glob.glob(DATA_PATH+"anomaly.*.json")
+    anomaly_list.sort(key=lambda x: int(x.split('.')[-2]))
+
+    for i in range(len(trace_list)):
+        foi = []
+        with open(foi_list[i], 'r') as f:
+            foi = json.load(f)
+        anomaly = []
+        with open(anomaly_list[i], 'r') as f:
+            anomaly = json.load(f)
+        trace = []
+        with open(trace_list[i], 'r') as f:
+            trace = json.load(f)
+        data_manager.set_FOI(foi)
+        data_manager.set_labels(anomaly)
+        trace.sort(key=lambda event:event[11])
+        data_manager.add_events(trace)
+        data_manager.generate_forest()
+
+def divide_by_rank(): 
+    """
+    Split aggregated data by rank
+    """
+    EXPAND_OFFSET = 0 # To be first rank
+    filecnt = 0
+
+    if MODE == 'events': # ../../data/nwchem_events/
+
+        trace_list = glob.glob(DATA_PATH+"trace.*.json")
+        trace_list.sort(key=lambda x: int(x.split('.')[-2]))
+
+        for i in range(len(trace_list)):
+            rank_map = {}
+            trace = {}
+            with open(trace_list[i], 'r') as f:
+                trace = json.load(f)
+
+            events = trace['value']['events']
+            for event in events:
+                event[1] = event[1] + EXPAND_OFFSET
+
+            for event in events:
+                rank_num = event[1]
+                if rank_num not in rank_map:
+                    rank_map[rank_num] = []
+                rank_map[rank_num].append(event)
+
+            for rank, rank_data in rank_map.items():
+                trace['value']['events'] = rank_data
+                j = json.dumps(trace)
+                f = open('rank.'+str(int(rank))+'.trace.'+str(filecnt)+'.json','w')
+                f.write(j)
+                f.close()
+            filecnt += 1
+
+    elif MODE == 'executions': # ../../data/nwchem_executions/ 
+        # load data
+        data_list = glob.glob(DATA_PATH+"trace.*.json")
+        data_list.sort(key=lambda x: int(x.split('.')[-2]))
+
+        eidmap = {}
+        eids = {}
+        
+        for i in range(len(data_list)):
+            data = []
+            with open(data_list[i], 'r') as f:
+                data = json.load(f)
+            executions = data['executions']
+            frame_data = {}
+            for eid, execution in executions.items():
+                
+                execution['comm ranks'] = execution['comm ranks'] + EXPAND_OFFSET
+                
+                rank_num = execution['comm ranks'] 
+                
+                if rank_num not in frame_data:
+                    frame_data[rank_num] = {}
+                # if rank_num not in eidmap:
+                #     eidmap[rank_num] = {}
+                if rank_num not in eids:
+                    eids[rank_num] = -1
+                
+                fid = str(execution['findex']) if type(execution['findex']) == int else execution['findex']
+                if fid not in eidmap:
+                    eidmap[fid] = eids[rank_num]
+                    eids[rank_num] -= 1
+                execution['findex'] = eidmap[fid]
+                
+                pid = str(execution['parent']) if type(execution['parent']) == int else execution['parent']
+                if pid not in eidmap:
+                    eidmap[pid] = eids[rank_num]
+                    eids[rank_num] -= 1
+                execution['parent'] = eidmap[pid]
+                
+                new_children = []
+                for cid in execution['children']:
+                    cid = str(cid) if type(cid) == int else cid
+                    if cid not in eidmap:
+                        eidmap[cid] = eids[rank_num]
+                        eids[rank_num] -= 1
+                    new_children.append(eidmap[cid])
+                execution['children'] = new_children
+                
+                frame_data[rank_num][eidmap[eid]] = execution
+
+            for rank, rank_data in frame_data.items():
+                data['executions'] = rank_data
+                j = json.dumps(data)
+                f = open('rank.'+str(int(rank))+'.trace.'+str(filecnt)+'.json','w')
+                f.write(j)
+                f.close()
+            filecnt += 1
+
+def update_statistics():
+    
+    data_list = glob.glob(DATA_PATH+"trace.*.json")
+    data_list.sort(key=lambda x: int(x.split('.')[-2]))
+
+    
+
+        
+
+if __name__ == "__main__":
+    update_old_events_to_trace_format()
+    # convert_events_to_executions()
+    # divide_by_rank()
--- test/parser/stream_parser.py
+++ test/parser/stream_parser.py
@@ -2,17 +2,17 @@ import requests
 import time
 import json
 import glob
-import _thread
 import time
+from threading import Thread, current_thread
+import sys
+import os
 
-AD_SEND_TYPE = 'per_rank' # aggregated or per_rank
-DATA_TYPE = 'executions' # events or executions
-DATA_PATH = '../data/per_rank/'
+AD_SEND_TYPE = 'AGGREGATED' # AGGREGATED or MULTI_STREAM
+DATA_TYPE = 'EXECUTIONS' # EVENTS or EXECUTIONS
+DATA_PATH = '../../data/aggregated/executions/' # ../../data/sc19/    ../../data/per_rank/ # ../../data/nwchem_executions_rank/ # ../../data/nwchem_events_rank/ # ../../data/aggregated/executions/ # ../../data/sc19/
 VIS_URL = 'http://0.0.0.0:5000/'
-NUM_RANK = 8
-
-# initialize server
-requests.post(VIS_URL, json={'type':'reset'})
+TRACE_FRAME_LIMIT = 1000
+NUM_RANK = 10
 
 def check_data():
 	cnt = 0
@@ -23,54 +23,130 @@ def check_data():
 	else: 
 		return True
 
-def send_data(tname, rank):
-    try:
-        while len(global_data[rank])>0:
-            d = global_data[rank].pop(0)
-            res = requests.post(VIS_URL+DATA_TYPE, json=d)
-            print('['+tname+']', res.json())
-    except:
-        print('Thread Error')
+thread_res_time = {}
+def send_data(rank):
+	try:
+		cnt = 0
+		global_time = 0
+		tot = len(global_data[rank])
+		while len(global_data[rank])>0:
+			if cnt < TRACE_FRAME_LIMIT:
+				d = global_data[rank].pop(0)
+				start = time.time()
+				res = requests.post(VIS_URL+DATA_TYPE.lower(), json=d)
+				global_time += (time.time()-start)
+				print('['+current_thread().name+'] sent', cnt,'out of', tot, 'data', res.json())
+				cnt += 1
+			else:
+				break
+			# time.sleep(1)
+		thread_res_time[current_thread().name] = global_time/cnt
+		print('['+current_thread().name+'] avg response time:', thread_res_time[current_thread().name] )
+	except:
+		print('Thread Error')
 
 global_data = {} # global
-if AD_SEND_TYPE == 'per_rank':
-
-	# load data
+def send_multiple_stream_by_rank():
+	# initialize server
+	requests.post(VIS_URL, json={'type':'reset'})
+	num_data = 0
 	for i in range(NUM_RANK):
-		path_list = glob.glob(DATA_PATH+'rank.'+str(i)+'.execution.*.json')
-		path_list.sort(key=lambda x: int(x.split('.')[-2]))
+		# path_list = glob.glob(DATA_PATH+'rank.'+str(i)+'.trace.*.json')
+		# path_list.sort(key=lambda x: int(x.split('.')[-2]))
+		path_list = glob.glob(DATA_PATH+'dump-'+get_three_digit(i)+'/trace.*.*.json')
+		path_list.sort(key=lambda x: int(x.split('.')[-3]))
 		for path in path_list:
 			with open(path, 'r') as f:
 				if i not in global_data:
 					global_data[i] = []
-				global_data[i].append(json.load(f))
+				data = json.load(f)	
+				if DATA_TYPE == 'EXECUTIONS':
+					num_data += len(data['executions'])
+				elif DATA_TYPE == 'EVENTS': # events		
+					num_data += len(data['value']['events'])
+				global_data[i].append(data)
 	try:
+		threads = []
 		for i in range(NUM_RANK):
-			_thread.start_new_thread(send_data, ('tid-'+str(i), i) )
+			thrd = Thread(target=send_data, args=[i,])
+			threads.append(thrd)
+		start = time.time()
+		for thrd in threads:
+			thrd.start() 
+		while check_data():
+			time.sleep(0.1)
+		tot_res = time.time()-start
+		for thrd in threads:
+			thrd.join()
 	except:
-		print ('Error: unable to start thread')
-	while check_data():
-		pass
+		print ('Threading Error')
+	sum_res = 0
+	for k, v in thread_res_time.items():
+		sum_res += v
+	print('Done')
+	print('----------------------------------------------------------------')
+	print('# RANKS: %d # %s: %d, AVG. RESPONSE: %f, TOTAL RESPONSE: %d, AVG. PROCESS: %f' %(NUM_RANK, DATA_TYPE, num_data, sum_res/len(thread_res_time.keys()), tot_res, num_data/tot_res) )
 
-	print('Simulation Done')
+def get_three_digit(digit):
+	digit_str = str(digit)
+	while len(digit_str) <3:
+		digit_str = '0'+digit_str 
+	return digit_str
 
-else:
+def send_aggregated_data():
+	
+	# initialize server
+	requests.post(VIS_URL, json={'type':'reset'})
+	
 	path_list = glob.glob(DATA_PATH+'trace.*.json')
 	path_list.sort(key=lambda x: int(x.split('.')[-2]))
+	
 	num_data = 0
-	start = time.time()
+	global_time = 0
 	for i in range(len(path_list)):
-		data = []
-		with open(path_list[i], 'r') as f:
-			data = json.load(f)
-		res = requests.post(VIS_URL+DATA_TYPE, json=data)
-		print(res.json())
-		if DATA_TYPE == 'events':
-			num_data += len(data['value']['events'])
-		else:
-			num_data += len(data['executions'])
+		if i < TRACE_FRAME_LIMIT:
+			data = []
+			with open(path_list[i], 'r') as f:
+				data = json.load(f)
+			start = time.time()	
+			res = requests.post(VIS_URL+DATA_TYPE.lower(), json=data)
+			global_time += (time.time()-start)
+			print(res.json())
+			if DATA_TYPE == 'EVENTS':
+				num_data += len(data['value']['events'])
+			else:
+				num_data += len(data['executions'])
 
 	# summary
-	duration = time.time()-start
+	print('Done')
 	print('----------------------------------------------------------------')
-	print('Duration:', duration, 'sec, ', '# events (or executions) :', num_data, ', data/s: ', num_data/duration)
+	print('# %s: %d, AVG. RESPONSE: %f, TOTAL RESPONSE: %d, AVG. PROCESS: %f' %(DATA_TYPE, num_data, global_time/len(path_list), global_time, num_data/global_time) )
+
+def get_data_size():
+	size_map = {}
+	accum_size = 0
+	for i in range(NUM_RANK):
+		path_list = glob.glob(DATA_PATH+'dump-'+get_three_digit(i)+'/trace.*.*.json')
+		path_list.sort(key=lambda x: int(x.split('.')[-3]))
+		for path in path_list:
+			with open(path, 'r') as f:
+				accum_size = accum_size + os.path.getsize(path)
+		if i in [0, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99]:
+			size_map[i] = accum_size
+	print('size:', size_map)
+
+if __name__ == "__main__":
+
+	if len(sys.argv) == 2:
+		try:
+			log = open(sys.argv[1], 'w')
+			sys.stdout = sys.stderr = log
+		except IOError:
+			pass
+	
+	if AD_SEND_TYPE == 'MULTI_STREAM':
+		send_multiple_stream_by_rank()
+		# get_data_size()
+	elif AD_SEND_TYPE == 'AGGREGATED':
+		send_aggregated_data()
+	

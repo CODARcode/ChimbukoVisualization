dbe07d48e1272162af0a587cdc4c1c1388ecae89
wyjeong <dev.wyjeong@gmail.com>
2019-08-15 17:20:12 +0900

    Adding generalized In-Situ components (2)
    - split rendering from controlling and modeling in the In-Situ components: streamview and historyview
    - add processData() which generates the expected format of barchart 
    - refined minor refactorings

--- web/static/js/historyview.js
+++ web/static/js/historyview.js
@@ -1,45 +1,23 @@
 class HistoryView extends BarChartView {
-    constructor(data, svg) {
-        super(data, svg, {
-            'width': componentLayout.HISTORYVIEW_WIDTH,
-            'height': componentLayout.HISTORYVIEW_HEIGHT
-        });
-        this.name = 'historyview'
-        this.detailed = d3.select("#selected_rank_no");
-        this.LINE_COLOR = 'steelblue'//'#ff8080'
-        this.SELECTED_LINE_COLOR = 'steelblue'//'#ff8080'
-        this.HOVER_LINE_COLOR = '#A8A3A3'//'#ff8080'
-        this.NON_SELECTED_LINE_COLOR = '#A8A3A3'//'#ddd'
-        this.xAxisLabel = 'Frame';
-        this.yAxisLabel = '#. Anomaly';
-        this._data = {};
-        this.margin = {top: 20, right: 50, bottom: 30, left: 50};
-        this.container_width = 1000;
-        this.container_height = 500;
-        this.content_width = this.container_width -this.margin.left -this.margin.right;
-        this.content_height = this.container_height -this.margin.top -this.margin.bottom;
-        this.rank_of_interest_labels = {};
-        this.NUM_FRAME = 50
-        this.svg
-            .attr('class', 'historyview_svg')
-            .attr('width', this.container_width)
-            .attr('height', this.container_height);
-        this.content_area = this.svg.append('g')
-            .attr('class', 'historyview_content_area')
-            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
-            .attr('width', this.content_width)
-            .attr('height', this.content_height);
-        this.xAxis = this.svg.append('g')
-            .attr('class', 'historyview_x_axis')
-            .attr('transform', 'translate('+this.margin.left+',' + (this.content_height+this.margin.top) + ')');
-        this.yAxis = this.svg.append('g')
-            .attr('class', 'historyview_y_axis')
-            .attr('transform', 'translate('+this.margin.left+',' + this.margin.top + ')');
 
-        var me = this
-        this.dynamic = false;
+    constructor(data, svg, name) {
+        super(data, svg, name, {
+            'width': componentLayout.HISTORYVIEW_WIDTH,
+            'height': componentLayout.HISTORYVIEW_HIEGHT
+        }, {
+            'top': componentLayout.HISTORYVIEW_MARGIN_TOP, 
+            'right': componentLayout.HISTORYVIEW_MARGIN_RIGHT, 
+            'bottom': componentLayout.HISTORYVIEW_MARGIN_BOTTOM, 
+            'left': componentLayout.HISTORYVIEW_MARGIN_LEFT
+        });
+        this.name = name
         this.rangeStart = 0
+        this.dynamic = false;
+        this.detailed = d3.select("#selected_rank_no");
         this.rank_no = d3.select("#history_start_no").node()
+        this.NUM_FRAME = 50
+        
+        var me = this
         d3.select("#static_btn").on("click", function(d) {
             me.dynamic = false
             me._update();
@@ -49,7 +27,6 @@ class HistoryView extends BarChartView {
             me._update();
         });
     }
-
     stream_update(){
         /**
          * Called whenever data has received from backend.
@@ -59,127 +36,42 @@ class HistoryView extends BarChartView {
             this._update(this.data.rankHistoryInfo)
         }
     }
-
-    _update(){
+    _update(selectedRankInfo, history){
         /**
          * Renders delta plot after data converting and scales adjustment
         **/
-        var rankInfo = this.data.rankHistoryInfo;
-        this.fillColor = rankInfo.fill
-        this.detailed.text('Selected Rank #: '+rankInfo.rank)
-        this._data = this.getHistoryData(rankInfo.rank)
-        this.adjust_scale();
-        this.draw();
+        
+        this.fillColor = selectedRankInfo.fill
+        this.detailed.text('Selected Rank #: '+selectedRankInfo.rank)
+        this.processed = this.processData(selectedRankInfo.rank, history)
+        this.render('Frame', '# anomalies', this.processed);
     }
-    getHistoryData(rankno) {
+    processData(selectedRankID, history) {
         /**
-         * Prepares proper format for rendering history view
-         * 
-         * the format is like below:
-         * result == {
-         *      frame_id: the number of anomalies of the specific rank
+         * Process proper format for rendering 
+         * Dynamically generate/process the given data to the expected format of barChart
+         * format == {
+         *      name of category == {
+         *          x: [] # list of x values
+         *          y: [] # list of y values
+         *      }
          * }
          */
-        var res = {}
-        var frames = this.data.history; // (from=='streamview')? this.data.renderingFrames:this.data.renderingFramesBottom
-        for(var frameno in frames) {
-            if(this.dynamic) { // consider window size
-                if (frameno > (this.data.frameID-this.NUM_FRAME)) {
-                    res[frameno] = frames[frameno][rankno]===undefined? 0: frames[frameno][rankno]
-                }
-            } else { // render from the beginning
-                if (frameno >= this.rangeStart) {
-                    res[frameno] = frames[frameno][rankno]===undefined? 0: frames[frameno][rankno]
-                }
-            }
+        var processed = {
+            selectedRankID: {'x':[], 'y':[]}, // x: frames, y: # anomalies
         }
-        return res
+        var res = {}
+        for(var frameID in history) {
+            processed.selectedRankID.x.append(frameID)
+            processed.selectedRankID.y.append(history[frameID])
+        }
+        return processed;
     }
-    get_y_max(d) {
-        return Math.max(...Object.values(d));
-    }
-    adjust_scale() {
-        this.xScale = d3.scaleBand().range([0, this.content_width]).domain(Object.keys(this._data).map(Number)).paddingInner(0.05);
-        this.yScale = d3.scaleLinear().range([this.content_height, 0]).domain([0, d3.max(Object.values(this._data))]);
-    }
-    draw() {
-        this._updateAxis();
-        this._drawBars();
-    }
-    _updateAxis() {
-        this.xAxis.selectAll('text.historyview_xLabel').remove();
-        this.yAxis.selectAll('text.historyview_yLabel').remove();
-        this.axisBottom = d3.axisBottom(this.xScale);
-        this.axisLeft = d3.axisLeft(this.yScale);
-        this.xAxis.call(this.axisBottom).selectAll("text")
-            .attr('transform', 'rotate(-90)')
-            .attr('x', -15)
-            .attr('y', -5)
-        this.xAxis
-            .append('text')
-                .attr('class', 'historyview_xLabel')
-                .attr('x', this.content_width/2)
-                .attr('y', 30)
-                .style('text-anchor', 'middle')
-                .text(this.xAxisLabel)
-                .attr('fill', 'black')
-                .style('font-weight', 'bold');
-        this.yAxis.call(this.axisLeft)
-            .append('text')
-                .attr('class', 'historyview_yLabel')
-                .attr('transform', 'rotate(-90)')
-                .attr('y', -42)
-                .attr('x', -this.content_height/2)
-                .attr('dy', '.71em')
-                .style('text-anchor', 'middle')
-                .text(this.yAxisLabel)
-                .attr('fill', 'black')
-                .style('font-weight', 'bold');
-    }
-    _drawBars() {
-        var me = this;
-        me._rank = -1;
-        this.content_area.selectAll('rect').remove() // remove
-        var barData = this.getBarData();
-        this.content_area.selectAll('rect')
-            .data(barData).enter()
-                .append("rect")
-                .style("fill", this.fillColor)
-                .attr("x", function(d) { 
-                    // console.log('rank #: '+d.rank+', x: '+me.xScale(d.rank))
-                    return me.xScale(d.rank)
-                })
-                .attr("width", me.xScale.bandwidth())
-                .attr("y", function(d) { 
-                    // console.log('value: '+d.value+', y: '+me.yScale(d.value))
-                    return me.yScale(d.value)
-                })
-                .attr("height", function(d) { 
-                    // console.log('content_height: '+me.content_height+', me.yScale(d.value): '+ me.yScale(d.value))
-                    return me.content_height - me.yScale(d.value); }
-                ).on('click', function(d) {
-                    me.getExecutionsByTime(me.notify.bind(me), {
-                        'rank': d.rank,
-                        'start': 0,
-                        'end': 0
-                    });
-                });
-        this.bars = this.content_area.selectAll('historyview_bar');
-    }
-    getBarData() {
-        var res = []
-        Object.keys(this._data).forEach(rank => {
-            res.push({
-                'rank': Number(rank),
-                'value': this._data[rank],
-            })
-        });
-        return res;
-    }
-    getExecutionsByTime(callback, data) {
+    getScatterLayout(params) {
+        var _callback = this.notify.bind(this)
         fetch('/scatterplot', {
             method: "POST",
-            body: JSON.stringify(data),
+            body: JSON.stringify(params),
             headers: {
                 "Content-Type": "application/json"
             },
@@ -187,7 +79,7 @@ class HistoryView extends BarChartView {
         }).then(response => response.json()
             .then(json => {
                 if (response.ok) {
-                    callback(json);
+                    _callback(json);
                     return json
                 } else {
                     return Promise.reject(json)
@@ -203,8 +95,85 @@ class HistoryView extends BarChartView {
         this.scatterview.update(layout);
     }
 }
-d3.selection.prototype.moveToFront = function() {
-    return this.each(function(){
-        this.parentNode.appendChild(this);
-    });
-};
+
+// get_y_max(d) {
+    //     return Math.max(...Object.values(d));
+    // }
+    // adjust_scale() {
+    //     this.xScale = d3.scaleBand().range([0, this.content_width]).domain(Object.keys(this.processed).map(Number)).paddingInner(0.05);
+    //     this.yScale = d3.scaleLinear().range([this.content_height, 0]).domain([0, d3.max(Object.values(this.processed))]);
+    // }
+    // draw() {
+    //     this._updateAxis();
+    //     this._drawBars();
+    // }
+    // _updateAxis() {
+    //     this.xAxis.selectAll('text.historyview_xLabel').remove();
+    //     this.yAxis.selectAll('text.historyview_yLabel').remove();
+    //     this.axisBottom = d3.axisBottom(this.xScale);
+    //     this.axisLeft = d3.axisLeft(this.yScale);
+    //     this.xAxis.call(this.axisBottom).selectAll("text")
+    //         .attr('transform', 'rotate(-90)')
+    //         .attr('x', -15)
+    //         .attr('y', -5)
+    //     this.xAxis
+    //         .append('text')
+    //             .attr('class', 'historyview_xLabel')
+    //             .attr('x', this.content_width/2)
+    //             .attr('y', 30)
+    //             .style('text-anchor', 'middle')
+    //             .text(this.xAxisLabel)
+    //             .attr('fill', 'black')
+    //             .style('font-weight', 'bold');
+    //     this.yAxis.call(this.axisLeft)
+    //         .append('text')
+    //             .attr('class', 'historyview_yLabel')
+    //             .attr('transform', 'rotate(-90)')
+    //             .attr('y', -42)
+    //             .attr('x', -this.content_height/2)
+    //             .attr('dy', '.71em')
+    //             .style('text-anchor', 'middle')
+    //             .text(this.yAxisLabel)
+    //             .attr('fill', 'black')
+    //             .style('font-weight', 'bold');
+    // }
+    // _drawBars() {
+    //     var me = this;
+    //     me._rank = -1;
+    //     this.content_area.selectAll('rect').remove() // remove
+    //     var barData = this.getBarData();
+    //     this.content_area.selectAll('rect')
+    //         .data(barData).enter()
+    //             .append("rect")
+    //             .style("fill", this.fillColor)
+    //             .attr("x", function(d) { 
+    //                 // console.log('rank #: '+d.rank+', x: '+me.xScale(d.rank))
+    //                 return me.xScale(d.rank)
+    //             })
+    //             .attr("width", me.xScale.bandwidth())
+    //             .attr("y", function(d) { 
+    //                 // console.log('value: '+d.value+', y: '+me.yScale(d.value))
+    //                 return me.yScale(d.value)
+    //             })
+    //             .attr("height", function(d) { 
+    //                 // console.log('content_height: '+me.content_height+', me.yScale(d.value): '+ me.yScale(d.value))
+    //                 return me.content_height - me.yScale(d.value); }
+    //             ).on('click', function(d) {
+    //                 me.getScatterLayout(me.notify.bind(me), {
+    //                     'rank': d.rank,
+    //                     'start': 0,
+    //                     'end': 0
+    //                 });
+    //             });
+    //     this.bars = this.content_area.selectAll('historyview_bar');
+    // }
+    // getBarData() {
+    //     var res = []
+    //     Object.keys(this.processed).forEach(rank => {
+    //         res.push({
+    //             'rank': Number(rank),
+    //             'value': this.processed[rank],
+    //         })
+    //     });
+    //     return res;
+    // }

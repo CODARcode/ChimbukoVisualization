990208731cb6eeda3234cd62033a9bd17a0bcb68
wyjeong <dev.wyjeong@gmail.com>
2019-08-28 18:27:09 +0900

    Adding unittest for frontend module (3)
    - refined minor bug

--- test/model.test.js
+++ test/model.test.js
@@ -5,7 +5,7 @@ describe('Ensuring data receiving from backend', () => {
     it('receives 10 ranks from backend', () => {
         console.log(data);
         let dataObject = new data();
-        dataObject._update(ranks)
+        dataObject.update(ranks)
         expect(dataObject.getRankIDList().length).toBe(10);
     });
 });
